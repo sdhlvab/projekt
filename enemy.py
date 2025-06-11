4a936ec8f6f5a74d47c4199416830a514d437272
sdhlvab <kamil.vortex@outlook.com>
2025-06-11 09:40:05 +0200

    fixed enemies moves

--- enemy.py
+++ enemy.py
@@ -1,4 +1,6 @@
 import pygame
+from select import select
+
 from config import TILE_SIZE
 
 class Enemy(pygame.sprite.Sprite):
@@ -12,25 +14,54 @@ class Enemy(pygame.sprite.Sprite):
         self.gravity = 0.7
         self.direction = 1
         self.speed = 2
-        self.on_ground = True
+        self.on_ground = False
 
     def update(self, ground_rects):
-        self.rect.x += self.direction * self.speed
-        self.apply_gravity()
+        # self.rect.x += self.direction * self.speed
+        # self.apply_gravity()
+        # if self.on_ground:
+        #     self.velocity.x = self.direction * self.speed
+        # else:
+        #     self.velocity.x = 0
+        # # Odbijaj się od krawędzi/ścian
+        # for tile in ground_rects:
+        #     if self.rect.colliderect(tile):
+        #         if self.direction > 0:
+        #             self.rect.right = tile.left
+        #         else:
+        #             self.rect.left = tile.right
+        #         self.direction *= -1
+
         if self.on_ground:
             self.velocity.x = self.direction * self.speed
         else:
             self.velocity.x = 0
-        # Odbijaj się od krawędzi/ścian
+
+        self.apply_gravity()
+
+        self.rect.x += self.velocity.x
         for tile in ground_rects:
             if self.rect.colliderect(tile):
-                if self.direction > 0:
+                if self.velocity.x > 0:
                     self.rect.right = tile.left
-                else:
+                    self.direction *= -1
+                elif self.velocity.x < 0:
                     self.rect.left = tile.right
-                self.direction *= -1
+                    self.direction *= -1
+
+        self.rect.y += self.velocity.y
+        self.on_ground = False
+        for tile in ground_rects:
+            if self.rect.colliderect(tile):
+                if self.velocity.y > 0:
+                    self.rect.bottom = tile.top
+                    self.velocity.y = 0
+                    self.on_ground = True
+                elif self.velocity.y < 0:
+                    self.rect.top = tile.bottom
+                    self.velocity.y = 0
 
     def apply_gravity(self):
         self.velocity.y += self.gravity
-        if self.velocity.y > 12:
-            self.velocity.y = 12
+        if self.velocity.y > 10:
+            self.velocity.y = 10
